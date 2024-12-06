3b2963b0a9ae1d9ea7b404df4f67dc9d9a478a4d
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-12-06 12:06:42 +0100

    feat: more ui improvements

--- packages/ui/src/components/custom/general/label.tsx
+++ packages/ui/src/components/custom/general/label.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState, useRef, useEffect } from "react";
 import { FaInfoCircle } from "react-icons/fa";
 import {
   Tooltip,
@@ -6,6 +8,10 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../../shadcn/tooltip";
+import { useScreenSize } from "../../../lib/hooks/use-screen-size";
+import { breakpoints } from "../../../lib/utils";
+import { cn } from "../../../lib/utils";
+import { useIsTouchDevice } from "../../../lib/hooks/use-is-touch-device";
 
 type LabelProps = {
   label: string;
@@ -15,25 +21,68 @@ type LabelProps = {
 };
 
 export function Label({ label, info, required, children }: LabelProps) {
+  const { isTouchDevice } = useIsTouchDevice();
+  const [open, setOpen] = useState(false);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        tooltipRef.current &&
+        !tooltipRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <label className="flex flex-col gap-1">
       <div className="flex flex-row gap-1 items-center">
-        <div className="text-xs text-darkish-text">
+        <div className="text-sm text-darkish-text">
           {label}
           {required ? "*" : ""}
         </div>
         {info !== undefined && (
           <TooltipProvider>
-            <Tooltip delayDuration={200}>
-              <TooltipTrigger type="button">
-                <FaInfoCircle className="text-light-text" size={14} />
-              </TooltipTrigger>
-              <TooltipContent
-                side="right"
-                className="max-w-[300px] relative z-[3]"
-              >
-                <p>{info}</p>
-              </TooltipContent>
+            <Tooltip delayDuration={isTouchDevice ? 0 : 200} open={open}>
+              <div ref={tooltipRef} className="flex flex-row items-center">
+                <TooltipTrigger
+                  type="button"
+                  onMouseEnter={() => {
+                    if (!isTouchDevice) {
+                      setOpen(true);
+                    }
+                  }}
+                  onMouseLeave={() => {
+                    if (!isTouchDevice) {
+                      setOpen(false);
+                    }
+                  }}
+                  onClick={() => {
+                    if (isTouchDevice) {
+                      setOpen(!open);
+                    }
+                  }}
+                >
+                  <FaInfoCircle
+                    className="text-light-text cursor-pointer"
+                    size={16}
+                    role="button"
+                    aria-label={`Info for ${label}`}
+                  />
+                </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  className="max-w-[150px] md:max-w-[300px] relative z-[3] text-sm md:text-xs"
+                  sideOffset={5}
+                >
+                  <p>{info}</p>
+                </TooltipContent>
+              </div>
             </Tooltip>
           </TooltipProvider>
         )}
--- packages/ui/src/lib/hooks/use-screen-size.ts
+++ packages/ui/src/lib/hooks/use-screen-size.ts
@@ -1,15 +1,21 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { useDebounce } from "./use-debounce";
 
 export function useScreenSize() {
   const [screenSize, setScreenSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 2000,
+    height: 2000,
   });
 
   const handleResize = useDebounce(() => {
     setScreenSize({ width: window.innerWidth, height: window.innerHeight });
   }, 300);
+
+  useEffect(() => {
+    setScreenSize({ width: window.innerWidth, height: window.innerHeight });
+  }, []);
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);
